aeda413efa56dc0bc513759baad634d1d7f2ee67
Elllkere <me.mr.dom@bk.ru>
2025-10-05 00:52:54 +0300

    Update zapret_imp.hpp

--- ZapretGUI/zapret/zapret_imp.hpp
+++ ZapretGUI/zapret/zapret_imp.hpp
@@ -616,7 +616,7 @@ void Zapret::getArgs(const std::string& id_name, std::string& args, const std::s
     {
         args = std::format("--wf-tcp=443 --wf-udp=443 ");
         args += std::format("--filter-udp=443 --hostlist=\"{}\\{}\" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=\"{}\\quic_initial_www_google_com.bin\" --new ", cur_path, txt, cur_path);
-        args += std::format("--hostlist=\"{}\\{}\" --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig", cur_path, txt);
+        args += std::format("--filter-udp=443 --filter-tcp=443 --hostlist=\"{}\\{}\" --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig", cur_path, txt);
     }
     else if (id_name == "cloudflare")
     {
@@ -741,7 +741,7 @@ void Zapret::getArgs(const std::string& id_name, std::string& args, const std::s
     }
     else if (id_name == "twitch")
     {
-        args = std::format("--wf-tcp=443 --wf-udp=443,65535 ");
+        args = std::format("--wf-tcp=443 --wf-udp=443 ");
 
         args += std::format("--filter-tcp=443 --hostlist=\"{}\\{}\" --dpi-desync=disorder2 --dpi-desync-split-pos=1,sniext+1,host+1,midsld-2,midsld,midsld+2,endhost-1 --new ", cur_path, txt);
         args += std::format("--filter-tcp=443 --hostlist=\"{}\\{}\" --dpi-desync=fake --dpi-desync-fooling=md5sig --new ", cur_path, txt);
